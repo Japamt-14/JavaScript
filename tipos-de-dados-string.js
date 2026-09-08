9637a6dbeb867f94d640683da8cca5e699405b25
Japamt-14 <enzopereira1014@gmail.com>
2026-09-08 17:19:37 -0300

    add javaaaaa

---
+++ tipos-de-dados-string.js
@@ -0,0 +1,3 @@
+let nome = "ana"; // String com aspas duplas
+let sobrenome = "Silva"; // String com aspas simples
+let saudacao = `Olá, ${nome}!`; // template literal com interpolação de variáveis1
